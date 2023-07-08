8d904c769c0a761b0d8b8f3f228c0ffb0d7d97ad
박다예 <sju02030@gmail.com>
2023-07-08 16:38:03 +0900

    chore: initial commit

---
+++ src/main/java/com/nexters/moyeomoyeo/MoyeoMoyeoApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.moyeomoyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeoMoyeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeoMoyeoApplication.class, args);
+	}
+
+}
