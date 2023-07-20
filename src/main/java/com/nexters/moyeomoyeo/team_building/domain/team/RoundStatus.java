2b5b9c67d4ac2d7b301c12daf51bdd4b70a74041
sujin.heo <sujin.heo@musinsa.com>
2023-07-20 09:08:29 +0900

    [MOYEO-13] 현재 상태에 대한 가중치 추

--- src/main/java/com/nexters/moyeomoyeo/team_building/domain/team/RoundStatus.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/domain/team/RoundStatus.java
@@ -1,9 +1,22 @@
 package com.nexters.moyeomoyeo.team_building.domain.team;
 
+import com.nexters.moyeomoyeo.team_building.domain.room.*;
+
 public enum RoundStatus {
-	FIRST_ROUND,
-	SECOND_ROUND,
-	THIRD_ROUND,
-	FORTH_ROUND,
-	COMPLETE
+	FIRST_ROUND(1),
+	SECOND_ROUND(2),
+	THIRD_ROUND(3),
+	FORTH_ROUND(4),
+	COMPLETE(5),
+	;
+
+	RoundStatus(int weight) {
+		this.weight = weight;
+	}
+
+	private int weight;
+
+	public boolean canNextRound(RoomStatus roomStatus) {
+		return !roomStatus.isFinishRound(this.weight);
+	}
 }
