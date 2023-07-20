20fe20daf7f41e1a2f423eb5f4005c29e8b8ded0
sujin.heo <sujin.heo@musinsa.com>
2023-07-20 09:09:10 +0900

    [MOYEO-13] Setter 제거 및 FetchType 변경

--- src/main/java/com/nexters/moyeomoyeo/team_building/domain/team/Team.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/domain/team/Team.java
@@ -6,7 +6,6 @@ import lombok.*;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 public class Team {
 	@Id
@@ -15,14 +14,38 @@ public class Team {
 	private Long id;
 	private String name;
 
-	@Column(name = "team_status")
+	@Column(name = "team_uuid")
 	private String uuid;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "round_status")
 	private RoundStatus roundStatus;
 
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id")
 	private Room room;
+
+	public Team(Long id, String name, String uuid, RoundStatus roundStatus, Room room) {
+		this.id = id;
+		this.name = name;
+		this.uuid = uuid;
+		this.roundStatus = roundStatus;
+		this.room = room;
+	}
+
+	public Team(Long id, String name, String uuid, Room room) {
+		this(id, name, uuid, RoundStatus.FIRST_ROUND, room);
+	}
+
+	public void changeRoomStatus(RoundStatus updateRoundStatus) {
+		this.roundStatus = updateRoundStatus;
+	}
+
+	public boolean addRoom(Room room) {
+		if (this.room == null) {
+			this.room = room;
+			return true;
+		}
+		return false;
+	}
 }
