314d434de8796946e77e4cb76d0ae4893b13a239
sujin.heo <sujin.heo@musinsa.com>
2023-07-20 09:08:08 +0900

    [MOYEO-13] Room status 내부 상태 외부에 들어내지 않도록 변경

--- src/main/java/com/nexters/moyeomoyeo/team_building/domain/room/Room.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/domain/room/Room.java
@@ -7,9 +7,10 @@ import lombok.*;
 
 import java.util.*;
 
+import static com.nexters.moyeomoyeo.team_building.domain.room.RoomStatus.FIRST_ROUND;
+
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 public class Room {
 	@Id
@@ -22,13 +23,38 @@ public class Room {
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "round_status")
-	private RoomStatus RoomStatus;
+	private RoomStatus roomStatus;
 
 	@OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
 	private List<Team> teams = new ArrayList<Team>();
 
 	@OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
 	private List<User> users = new ArrayList<User>();
+
+	public Room(Long id, String name, String entrance_uri, RoomStatus roomStatus, List<Team> teams, List<User> users) {
+		this.id = id;
+		this.name = name;
+		this.entrance_uri = entrance_uri;
+		this.roomStatus = roomStatus;
+		this.teams = teams;
+		this.users = users;
+	}
+
+	public Room(Long id, String name, String entrance_uri, List<Team> teams, List<User> users) {
+		this(id, name, entrance_uri, FIRST_ROUND, teams, users);
+	}
+
+	public void addUsers(List<User> users) {
+		users.addAll(users);
+	}
+
+	public void addTeams(List<Team> teams) {
+		teams.addAll(teams);
+	}
+
+	public void changeRoomStatus(RoomStatus updatedRoomStatus) {
+		this.roomStatus = updatedRoomStatus;
+	}
 }
 
 
