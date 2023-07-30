2a6db93a9c1326b4df0a16b270a8fe08b695b97e
sujin.heo <sujin.heo@musinsa.com>
2023-07-30 20:53:38 +0900

    [MOYEO-32] 컬럼 설명 추가

--- src/main/java/com/nexters/moyeomoyeo/team_building/controller/dto/RoomInfoResponse.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/controller/dto/RoomInfoResponse.java
@@ -3,6 +3,8 @@ package com.nexters.moyeomoyeo.team_building.controller.dto;
 import com.nexters.moyeomoyeo.team_building.domain.constant.Position;
 import com.nexters.moyeomoyeo.team_building.domain.constant.RoundStatus;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,8 +17,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RoomInfoResponse {
 
+	@Schema(description = "팀 빌딩 정보")
 	private RoomInfo roomInfo;
+	@Schema(description = "팀빌딩에 참여하는 팀 정보")
 	private List<TeamInfo> teamInfoList;
+	@Schema(description = "팀빌딩에 참여하는 회원 정보")
 	private List<UserInfo> userInfoList;
 
 	@Getter
@@ -24,8 +29,9 @@ public class RoomInfoResponse {
 	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	@Builder
 	public static class RoomInfo {
-
+		@Schema(description = "팀 빌딩 방 고유값")
 		private String roomUrl;
+		@Schema(description = "팀 빌딩 진행 상태")
 		private RoundStatus roundStatus;
 	}
 }
--- src/main/java/com/nexters/moyeomoyeo/team_building/controller/dto/TeamInfo.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/controller/dto/TeamInfo.java
@@ -1,6 +1,7 @@
 package com.nexters.moyeomoyeo.team_building.controller.dto;
 
 import com.nexters.moyeomoyeo.team_building.domain.constant.*;
+import io.swagger.v3.oas.annotations.media.*;
 import lombok.*;
 
 @Getter
@@ -8,11 +9,15 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class TeamInfo {
-
+	@Schema(description = "팀 고유값")
 	private String uuid;
+	@Schema(description = "팀 명")
 	private String teamName;
+	@Schema(description = "PM 이름")
 	private String pmName;
+	@Schema(description = "PM 포지션")
 	private Position pmPosition;
+	@Schema(description = "팀원 선택이 완료되었는지 상태")
 	private boolean isSelectDone;
 
 	public static boolean isSelectDone(RoundStatus roomStatus, RoundStatus teamStatus) {
