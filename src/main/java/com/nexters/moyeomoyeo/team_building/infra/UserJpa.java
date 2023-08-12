9a4b78b823fa4d89f19ce895f53df0a4ce1efbf0
허수진/회원서비스팀 <sujin.heo@musinsa.com>
2023-08-12 23:03:17 +0900

    [MOYEO-51] 삭제 로직 구현

--- src/main/java/com/nexters/moyeomoyeo/team_building/infra/UserJpa.java
+++ src/main/java/com/nexters/moyeomoyeo/team_building/infra/UserJpa.java
@@ -1,10 +1,13 @@
 package com.nexters.moyeomoyeo.team_building.infra;
 
 import com.nexters.moyeomoyeo.team_building.domain.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserJpa extends JpaRepository<User, Long> {
 
 	Optional<User> findByUuid(String uuid);
+
+	void delete(User user);
 }
