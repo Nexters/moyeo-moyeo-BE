030dbbb4622627497d1853522a34a9c02ad6532d
박다예 <sju02030@gmail.com>
2023-08-18 23:45:07 +0900

    fix: adjust round 오류 수정

--- src/main/java/com/nexters/moyeomoyeo/common/config/SwaggerConfig.java
+++ src/main/java/com/nexters/moyeomoyeo/common/config/SwaggerConfig.java
@@ -3,15 +3,15 @@ package com.nexters.moyeomoyeo.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-	info = @Info(title = "모여모여 API 명세서",
-		description = "넥스터즈 팀빌딩 서비스 API 명세서",
-		version = "v1"))
+	info = @Info(title = "모여모여 API 명세서", description = "넥스터즈 팀빌딩 서비스 API 명세서", version = "v1"),
+	servers = @Server(url = "/"))
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
